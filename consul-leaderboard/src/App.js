e23d08f19534ff5219ce187d2cc433a4117e0c91
Addison van den Hoeven <AVANDENHOEVE14@ubishops.ca>
2019-01-16 13:01:03 -0500

    Added dropdown menu

--- consul-leaderboard/src/App.js
+++ consul-leaderboard/src/App.js
@@ -10,7 +10,12 @@ import Leaderboard from "./components/Leaderboard"
 import {
   Container,
   Row,
-  Col
+  Col, 
+  Dropdown, 
+  DropdownToggle, 
+  DropdownMenu, 
+  DropdownItem,
+  InputGroupAddon
 } from "reactstrap";
 
 
@@ -47,17 +52,17 @@ const QUERY_DATA_DEBATES = gql`
 const QUERY_DATA_PROPOSALS = gql`
 {
   proposals {
-    edges{
-      node{
+    edges {
+      node {
         public_created_at
-        public_author{
+        public_author {
           username
         }
         comments_count
         comments {
           edges {
-            node{
-              public_author{
+            node {
+              public_author {
                 username
               }
               cached_votes_up
@@ -77,8 +82,8 @@ const QUERY_BOTH = gql`
 {  
   proposals: 
     proposals {
-      edges{
-        node{
+      edges {
+        node {
           public_created_at
           public_author{
             username
@@ -90,7 +95,7 @@ const QUERY_BOTH = gql`
     }
 	
   debates: 
-    debates{
+    debates {
       edges {
         node {
           public_created_at
@@ -108,10 +113,10 @@ const QUERY_BOTH = gql`
 `
 const QUERY_COMMENTS = gql`
 { 
-  comments{
-    edges{
-      node{
-        public_author{
+  comments {
+    edges {
+      node {
+        public_author {
           username
         }
         commentable_type
@@ -124,52 +129,147 @@ const QUERY_COMMENTS = gql`
 `
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.setTime = this.setTime.bind(this);
+    this.setData = this.setData.bind(this);
+    this.setSort = this.setSort.bind(this);
+    this.getName = this.getName.bind(this);
+    this.chooseQuery = this.chooseQuery.bind(this);
+
+    this.state = {
+      timeDropdownOpen: false,
+      dataDropdownOpen: false,
+      sortDropdownOpen: false,
+      currentTime: "all_time",
+      currentData: "both",
+      currentSort: "activity"
+    };
+  }
+
+  toggle(menuName) {
+    switch(menuName) {
+      case "time": this.setState(prevState => ({timeDropdownOpen: !prevState.timeDropdownOpen})); break;
+      case "data": this.setState(prevState => ({dataDropdownOpen: !prevState.dataDropdownOpen})); break;
+      case "sort": this.setState(prevState => ({sortDropdownOpen: !prevState.sortDropdownOpen})); break;
+      default: break;
+    }
+  }
+
+  setTime(value) {
+    this.setState({
+      currentTime: value
+    });
+  }
+
+  setData(value) {
+    this.setState({
+      currentData: value
+    });
+  }
+
+  setSort(value) {
+    this.setState({
+      currentSort: value
+    });
+  }
+
+  getName(key) {
+    var legend = {
+      "all_time": "All time",
+      "this_week": "This week",
+      "debates": "Debates",
+      "proposals": "Proposals",
+      "both": "All",
+      "activity": "Activity",
+      "most_beloved": "Most Popular",
+      "most_controversial": "Most Controversial"
+    };
+    return(legend[key]);
+  }
+
+  chooseQuery() {
+    switch(this.state.currentData) {
+      case "both": return QUERY_BOTH;
+      case "debates": return QUERY_DATA_DEBATES;
+      case "proposals": return QUERY_DATA_PROPOSALS;
+      default: break;
+    }
+  }
+
   render () {
     return (
-      <Query query={QUERY_BOTH}>
-      {({ loading: loadingOne, error: errorOne, data: both }) => (
-        <Query query={QUERY_COMMENTS}>
-          {({ loading: loadingTwo, error: errorTwo, data: comments}) => {
-            if (loadingOne || loadingTwo) return (<div className="lds-ripple"><div></div><div></div></div>);
-            if (errorOne || errorTwo) return `Error!: ${errorOne}`;
-            return( 
-              <Container className="App" fluid>
-                  <Row>
-                    <Col md="12">
-                      <Leaderboard debates={both.debates.edges} proposals={both.proposals.edges} comments = {comments.comments.edges}></Leaderboard>;
-                    </Col>
-                  </Row>
-              </Container>
-            )
-          }}
-        </Query>
-      )}
-    </Query>
+      <Container fluid>
+        <Row>
+          <Col md="8"></Col>
+          <Col md="4">
+            <Row>
+              <InputGroupAddon addonType="prepend" style={{marginLeft: "20px"}}>When: </InputGroupAddon>
+              <Dropdown isOpen={this.state.timeDropdownOpen} toggle={() => this.toggle("time")}>
+                <DropdownToggle caret>
+                  {this.getName(this.state.currentTime)}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem onClick={() => this.setTime("all_time")}>All time</DropdownItem>
+                  <DropdownItem onClick={() => this.setTime("this_week")}>This week</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              <InputGroupAddon addonType="prepend" style={{marginLeft: "20px"}}>Data: </InputGroupAddon>
+              <Dropdown isOpen={this.state.dataDropdownOpen} toggle={() => this.toggle("data")}>
+                <DropdownToggle caret>
+                  {this.getName(this.state.currentData)}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem onClick={() => this.setData("both")}>All</DropdownItem>
+                  <DropdownItem onClick={() => this.setData("debates")}>Debates</DropdownItem>
+                  <DropdownItem onClick={() => this.setData("proposals")}>Proposals</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              <InputGroupAddon addonType="prepend" style={{marginLeft: "20px"}}>Sort By: </InputGroupAddon>
+              <Dropdown isOpen={this.state.sortDropdownOpen} toggle={() => this.toggle("sort")}>
+                <DropdownToggle caret>
+                  {this.getName(this.state.currentSort)}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem onClick={() => this.setSort("activity")}>Activity</DropdownItem>
+                  <DropdownItem onClick={() => this.setSort("most_beloved")}>Most Popular</DropdownItem>
+                  <DropdownItem onClick={() => this.setSort("most_controversial")}>Most Controversial</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </Row>
+          </Col>
+        </Row>
+        <Query query={this.chooseQuery()}>
+        {({ loading: loadingOne, error: errorOne, data: data }) => (
+          <Query query={QUERY_COMMENTS}>
+            {({ loading: loadingTwo, error: errorTwo, data: comments}) => {
+              if (loadingOne || loadingTwo) return (<div className="lds-ripple"><div></div><div></div></div>);
+              if (errorOne || errorTwo) return `Error!: ${errorOne}`;
+              console.log(this.state);
+              return( 
+                <Row>
+                  <Col md="12">
+                    <Leaderboard 
+                      debates={(this.state.currentData === "both" || this.state.currentData === "debates") ? data.debates.edges : []}
+                      proposals={(this.state.currentData === "both" || this.state.currentData === "proposals") ? data.proposals.edges : []} 
+                      comments = {comments.comments.edges}
+                      sort_filter= {this.state.currentSort}
+                      contents_filter= {this.state.currentData}
+                      time_filter= {this.state.currentTime}
+                    />
+                  </Col>
+                </Row>
+              )
+            }}
+          </Query>
+        )}
+      </Query>
+    </Container>
     )
   }
-  // render() {
-  //   return (
-  //     <Query query={QUERY_BOTH}>
-  //       {
-  //       ({ loading, error, data, comments }) => {
-  //         if (loading) return (<div className="lds-ripple"><div></div><div></div></div>);
-  //         if (error) return `Error!: ${error}`;
-  //         if (data !== {}) {          
-  //           return (
-  //             <Container className="App" fluid>
-  //               <Row>
-  //                 <Col md="12">
-  //                   <Leaderboard debates={data.debates.edges} proposals={data.proposals.edges}></Leaderboard>;
-  //                 </Col>
-  //               </Row>
-  //             </Container>
-  //           );
-  //         }
-  //       }
-  //     }
-  //   </Query>
-  //   );
-  // }
 }
 
 export default App;
