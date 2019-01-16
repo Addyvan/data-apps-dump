15c15eddd96826029ab25b8af19828b2caf567d5
Beatrice084 <bjohn084@uottawa.ca>
2019-01-16 15:19:07 -0500

    fixed column spacing

--- consul-leaderboard/src/App.js
+++ consul-leaderboard/src/App.js
@@ -206,7 +206,7 @@ class App extends Component {
       <Container fluid>
         <Row>
           <Col md="8"></Col>
-          <Col md="4">
+          <Col md="8">
             <Row>
               <InputGroupAddon addonType="prepend">When: </InputGroupAddon>
               <Dropdown isOpen={this.state.timeDropdownOpen} toggle={() => this.toggle("time")}>
